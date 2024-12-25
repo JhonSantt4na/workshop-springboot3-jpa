eb1e55dde50342dbcf9f29e79add7b911f96fd20
Santt4naDev <jhonnsantt4na@gmail.com>
2024-12-25 09:36:20 -0300

    Project Created

---
+++ src/test/java/com/santt4naweb/workshop01/Workshop01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santt4naweb.workshop01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
